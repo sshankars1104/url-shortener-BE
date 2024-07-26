d0ebe8f3e48afe8ab6c3dd5791dcf3dd964952a6
sshankars1104 <sshankars.mech@gmail.com>
2024-07-26 11:10:49 +0530

    Configure CORS

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://url-shortener-be-xqje.onrender.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
